7983cd972d7186e4c2409e196a0b0e6a052429b4
Minhaj28 <mdminhajuddin.official@gmail.com>
2024-01-12 19:49:20 +0600

    setup project folder structure

---
+++ src/services/auth.service.ts
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Service() {
+  const result = 42; // Replace 42 with the actual value you want
+
+  return result;
+}
+
+export default Service
---
+++ src/utils/jwt.ts
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function jwt() {
+    const result = 42; // Replace 42 with the actual value you want
+
+  return result;
+}
+
+export default jwt
